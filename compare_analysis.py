4de1b282d764c950b5369f6884a90444cc0599c9
Lauren Benson <lvbenson@iu.edu>
2020-03-09 19:37:35 -0400

    comparing analyses

--- compare_analysis.py
+++ compare_analysis.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import sys
 
 
-#v iz to compare: (1) variance against lesions. (2) mutual information against lesions.
+#viz to compare: (1) variance against lesions. (2) mutual information against lesions.
 # Lesions = "ground truth"
 
 dir = str(sys.argv[1])
@@ -19,6 +19,7 @@ viz = int(sys.argv[3])
 
 #load lesions data
 ip_lesion = np.load("./{}/Lesions_IP_{}.npy".format(dir,ind))
+#print(ip_lesion[0])
 cp_lesion = np.load("./{}/Lesions_CP_{}.npy".format(dir,ind))
 lw_lesion = np.load("./{}/Lesions_LW_{}.npy".format(dir,ind))
 
@@ -52,13 +53,63 @@ if viz == 1:
     #plt.plot(lw_mi, color="red",marker = "s", linestyle = 'none')
     plt.xlabel("Interneuron")
     plt.ylabel("Performance")
-    plt.title("Variance: Predictive Measure")
+    plt.title("Lesions vs Variance vs MutualInfo")
     plt.savefig(dir+"/Compare_"+str(ind)+".png")
     plt.show()
+    plt.close()
 
 #Finer-grain comparison
 #variance vs infolesion
-    
+#10 neurons, 2 hidden layers 
 
+
+    figure, axes = plt.subplots(nrows=2, ncols=5)
+
+    axes[0, 0].plot(ip_lesion[5],color = "purple",marker="o")
+    axes[0, 0].plot(ip_var[5],color="yellow",marker="^")
+    axes[0,0].title.set_text('HL 1, Neuron 1')
+    
+    axes[0,1].plot(ip_lesion[6],color = "purple",marker="o")
+    axes[0,1].plot(ip_var[6],color="yellow",marker="^")
+    axes[0,1].title.set_text('HL 1, Neuron 2')
+    
+    axes[0,2].plot(ip_lesion[7],color = "purple",marker="o")
+    axes[0,2].plot(ip_var[7],color="yellow",marker="^")
+    axes[0,2].title.set_text('HL 1, Neuron 3')
+    
+    axes[0,3].plot(ip_lesion[8],color = "purple",marker="o")
+    axes[0,3].plot(ip_var[8],color="yellow",marker="^")
+    axes[0,3].title.set_text('HL 1, Neuron 4')
+    
+    axes[0,4].plot(ip_lesion[9],color = "purple",marker="o")
+    axes[0,4].plot(ip_var[9],color="yellow",marker="^")
+    axes[0,4].title.set_text('HL 1, Neuron 5')
+    
+    axes[1,0].plot(ip_lesion[0],color = "purple",marker="o")
+    axes[1,0].plot(ip_var[0],color="yellow",marker="^")
+    axes[1,0].title.set_text('HL 2, Neuron 1')
+    
+    axes[1,1].plot(ip_lesion[1],color = "purple",marker="o")
+    axes[1,1].plot(ip_var[1],color="yellow",marker="^")
+    axes[1,1].title.set_text('HL 2, Neuron 2')
+    
+    axes[1,2].plot(ip_lesion[2],color = "purple",marker="o")
+    axes[1,2].plot(ip_var[2],color="yellow",marker="^")
+    axes[1,2].title.set_text('HL 2, Neuron 3')
+    
+    axes[1,3].plot(ip_lesion[3],color = "purple",marker="o")
+    axes[1,3].plot(ip_var[3],color="yellow",marker="^")
+    axes[1,3].title.set_text('HL 2, Neuron 4')
+    
+    axes[1,4].plot(ip_lesion[4],color = "purple",marker="o")
+    axes[1,4].plot(ip_lesion[4],color="yellow",marker="^")
+    axes[1,4].title.set_text('HL 2, Neuron 5')
+
+    #axes[2,5].title.set_text('lesions versus variance')
+
+    figure.tight_layout()
+    figure.suptitle('lesions versus variance: IP Task')
+    plt.show()
+    
     
 
